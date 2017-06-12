1c81a0b11939ae334c494cd95738657676d498d6
Narshimus <nash.williams@g.austincc.edu>
2017-06-12 17:56:18 -0500

    fixed seeding issue //.onDelete("CASCADE")

--- migrations/20170612144321_authors_books.js
+++ migrations/20170612144321_authors_books.js
@@ -1,8 +1,8 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('authors_books', (table) => {
     table.increments('id').primary();
-    table.integer('books_id').unsigned().references('id').inTable('books');
-    table.integer('authors_id').unsigned().references('id').inTable('authors');
+    table.integer('books_id').unsigned().references('id').inTable('books').onDelete('CASCADE');
+    table.integer('authors_id').unsigned().references('id').inTable('authors').onDelete('CASCADE');
   })
 };
 
