449806a128a4800a86a7a0f7428f1a3c031b7878
Narshimus <nash.williams@g.austincc.edu>
2017-06-14 14:53:36 -0500

    update routes/templates for viewing individual books

--- routes/books.js
+++ routes/books.js
@@ -7,37 +7,47 @@ const db = require('../knex')
 
 router.get('/books', (_req, res) => {
   getBooksAndAuthors().then((results) => {
-    results[0].forEach((book)=>{
-      let authorArray = results[1].filter((author)=> author.id === book.id);
-      book.authors = authorArray;
-    })
     res.render('pages/books', {
-      books: results[0]
+      books: results
     });
   });
 });
 
-router.get('/books/:index', (_req,res) => {
+router.get('/books/:index', (_req, res) => {
+  const index = Number.parseInt(_req.params.index);
+  if (Number.isNaN(index) || index < 0) {
+    next();
+  }
 
-})
+  getBooksAndAuthors(index).then((results) => {
+    res.render('pages/book', {
+      book: results
+    });
+  });
+});
 
-function getBooksAndAuthors(){
+function getBooksAndAuthors(index) {
+  index = index || -1;
   return Promise.all([
     getBooks(),
     getAuthors()
-  ]).then((results)=>{
-    return results;
+  ]).then((results) => {
+    results[0].forEach((book) => {
+      let authorArray = results[1].filter((author) => author.id === book.id);
+      book.authors = authorArray;
+    })
+    return index < 0 ? results[0] : results[0][index-1];
   })
 }
 
-function getBooks(){
+function getBooks() {
   return db('books').select('*');
 }
 
 function getAuthors() {
-  return db('authors').select('books.id','first_name')
-  .innerJoin('authors_books','authors_books.authors_id','authors.id')
-  .innerJoin('books','authors_books.books_id','books.id');
+  return db('authors').select('books.id', 'first_name')
+    .innerJoin('authors_books', 'authors_books.authors_id', 'authors.id')
+    .innerJoin('books', 'authors_books.books_id', 'books.id');
 };
 
 module.exports = router;
